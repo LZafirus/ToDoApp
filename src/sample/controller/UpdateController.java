ac5769dab228813dd4c012e356a726d3903bc374
owca <walaszczyk.dawid@gmail.com>
2020-10-24 20:46:25 +0200

    Update tasks process creation.

--- src/sample/controller/UpdateController.java
+++ src/sample/controller/UpdateController.java
@@ -1,4 +1,44 @@
 package sample.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class UpdateController {
+
+    @FXML
+    private TextField updateTaskName;
+
+    @FXML
+    private TextField updateTaskDesc;
+
+    @FXML
+    private Button updateSaveButton;
+
+    @FXML
+    private Label updateLabel;
+
+    @FXML
+    void initialize() {
+
+
+    }
+
+    public void setTaskName(String taskName) {
+        this.updateTaskName.setText(taskName);
+    }
+
+    public String getTaskName() {
+        return this.updateTaskName.getText().trim();
+    }
+
+    public void setUpdateTaskDesc(String taskDesc) {
+        this.updateTaskDesc.setText(taskDesc);
+    }
+
+    public String getTaskDesc() {
+        return this.updateTaskDesc.getText().trim();
+    }
+
 }
