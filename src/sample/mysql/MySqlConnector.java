9aeeac0a1f6a3a425031b43d23b580b987abd13a
Dawid Walaszczyk <63964259+WalaszczykDawid@users.noreply.github.com>
2021-07-17 00:06:27 +0200

    Sec update.

--- src/sample/mysql/MySqlConnector.java
+++ src/sample/mysql/MySqlConnector.java
@@ -9,10 +9,10 @@ import java.sql.*;
 
 public class MySqlConnector {
 
-    private final String url = "jdbc:mysql://localhost:3306/machinetodo1";
-    private final String user = "root";
-    private final String password = "";
-    private final String dataBase = "machinetodo1";
+    private final String url = "xxxxx";
+    private final String user = "xxxx";
+    private final String password = "xxx";
+    private final String dataBase = "xxxx";
 
     Connection connection;
 
