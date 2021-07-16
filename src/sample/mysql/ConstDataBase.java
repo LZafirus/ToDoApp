17a761101597544152be8939c0d6967bee0d7c62
Dawid Walaszczyk <63964259+WalaszczykDawid@users.noreply.github.com>
2021-07-17 00:09:03 +0200

    Sec changes.

--- src/sample/mysql/ConstDataBase.java
+++ src/sample/mysql/ConstDataBase.java
@@ -10,8 +10,8 @@ public class ConstDataBase {
     public static final String usersId = "user_id";
     public static final String usersName = "name";
     public static final String usersSurname = "surname";
-    public static final String usersLogin = "login";
-    public static final String usersPassword = "password";
+    public static final String usersLogin = "xxxxxxx";
+    public static final String usersPassword = "xxxxx";
 
     public static final String tasksTaskId = "task_id";
     public static final String tasksUserId = "user_id";
